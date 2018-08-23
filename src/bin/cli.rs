fe1c6d6d8bf9d121db4db69badcece4c5a00400c
Kaizhao Zhang <zhangkaizhao@gmail.com>
2018-08-23 22:32:32 +0800

    Remove one line functions

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -4,26 +4,6 @@ extern crate repos;
 
 use std::env;
 
-fn proxy(_repos: &repos::repos::Repos) {
-    _repos.proxy()
-}
-
-fn sync(_repos: &repos::repos::Repos, url: &str) {
-    _repos.sync(url)
-}
-
-fn remove(_repos: &repos::repos::Repos, url: &str) {
-    _repos.remove(url)
-}
-
-fn topics(_repos: &repos::repos::Repos) {
-    _repos.topics()
-}
-
-fn topic(_repos: &repos::repos::Repos, url: &str) {
-    _repos.topic(url)
-}
-
 fn main() {
     let _repos = repos::repos::Repos::new();
     let args: Vec<String> = env::args().collect();
@@ -32,8 +12,8 @@ fn main() {
         2 => {
             let subcommand = &args[1];
             match &subcommand[..] {
-                "proxy" => proxy(&_repos),
-                "topics" => topics(&_repos),
+                "proxy" => _repos.proxy(),
+                "topics" => _repos.topics(),
                 _ => println!("Sorry, not implemented yet!"),
             }
         },
@@ -41,12 +21,12 @@ fn main() {
             let subcommand = &args[1];
             let argument = &args[2];
             match &subcommand[..] {
-                "sync" => sync(&_repos, &argument),
-                "remove" => remove(&_repos, &argument),
-                "topic" => topic(&_repos, &argument),
+                "sync" => _repos.sync(&argument),
+                "remove" => _repos.remove(&argument),
+                "topic" => _repos.topic(&argument),
                 _ => println!("Sorry, not implemented yet!"),
             }
         },
-        _ => println!("Usage: repos subcommand [arguments]"),
+        _ => println!("Usage: repos subcommand [argument]"),
     }
 }
