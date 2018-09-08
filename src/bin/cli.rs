4519b31a516db4e2927b2aa4c53cbd213a3dac82
Kaizhao Zhang <zhangkaizhao@gmail.com>
2018-09-08 22:09:11 +0800

    Use clap crate instead of parse manually

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,10 +1,67 @@
+#[macro_use]
+extern crate clap;
 extern crate toml;
 
 extern crate repos;
 
-use std::env;
+use clap::{App, AppSettings, Arg, SubCommand};
 
 fn main() {
+    let app = App::new("repos")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("sync")
+                .about("update or clone if a repository url provided, else sync all repositories")
+                .arg(
+                    Arg::with_name("url")
+                        .help("repository url")
+                        .index(1)
+                        .required(false),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("remove")
+                .about("remove directory of a local repository")
+                .arg(
+                    Arg::with_name("url")
+                        .help("repository url")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("topics")
+                .about("list all topics with count of their repositories"),
+        )
+        .subcommand(
+            SubCommand::with_name("topic")
+                .about("list repositories of a topic")
+                .arg(
+                    Arg::with_name("topic")
+                        .help("topic")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .subcommand(SubCommand::with_name("stats").about("output stats of all repositories"))
+        .subcommand(SubCommand::with_name("cleanup").about("clean up broken repositories"))
+        .subcommand(
+            SubCommand::with_name("search")
+                .about("search local repositories by key word")
+                .arg(
+                    Arg::with_name("keyword")
+                        .help("key word to search")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .subcommand(SubCommand::with_name("proxy").about("output proxy configuration"));
+
+    let matches = app.get_matches();
+
     let manager = match repos::manager::Manager::new() {
         Ok(manager) => manager,
         Err(err) => {
@@ -12,31 +69,22 @@ fn main() {
             return ();
         }
     };
-    let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        2 => {
-            let subcommand = &args[1];
-            match &subcommand[..] {
-                "sync" => manager.sync_all(),
-                "topics" => manager.topics(),
-                "stats" => manager.stats(),
-                "cleanup" => manager.cleanup(),
-                "proxy" => manager.proxy(),
-                _ => unimplemented!(),
+    match matches.subcommand() {
+        ("sync", Some(sub_matches)) => {
+            if let Some(url) = sub_matches.value_of("url") {
+                manager.sync(url);
+            } else {
+                manager.sync_all();
             }
         }
-        3 => {
-            let subcommand = &args[1];
-            let argument = &args[2];
-            match &subcommand[..] {
-                "sync" => manager.sync(&argument),
-                "remove" => manager.remove(&argument),
-                "topic" => manager.topic(&argument),
-                "search" => manager.search(&argument),
-                _ => unimplemented!(),
-            }
-        }
-        _ => println!("Usage: repos subcommand [argument]"),
+        ("remove", Some(sub_matches)) => manager.remove(sub_matches.value_of("url").unwrap()),
+        ("topics", Some(_sub_matches)) => manager.topics(),
+        ("topic", Some(sub_matches)) => manager.topic(sub_matches.value_of("topic").unwrap()),
+        ("stats", Some(_sub_matches)) => manager.stats(),
+        ("cleanup", Some(_sub_matches)) => manager.cleanup(),
+        ("search", Some(sub_matches)) => manager.search(sub_matches.value_of("keyword").unwrap()),
+        ("proxy", Some(_sub_matches)) => manager.proxy(),
+        (_, _) => unreachable!(),
     }
 }
